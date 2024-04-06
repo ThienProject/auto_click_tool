19dab775873546874bb5d2e3ae404c5b05372278
vanthienpham <vanthien.dev@gmail.com>
2024-04-06 09:19:00 +0700

    test click on img'

--- jumping_dinosaur.py
+++ jumping_dinosaur.py
@@ -1,7 +1,14 @@
 import pyautogui as pag
 import time
 # image = 'cactus.png'
-image = "setting.png"
-findImage = pag.locateOnScreen("excel.png")
-print((findImage))
-# pag.click(findImage)
+# image = "setting.png"
+# image = "extention.png"
+image = 'excel2.png'
+# image = 'docker.png'
+pag.useImageNotFoundException()
+try:
+    findImage = pag.locateOnScreen(image, confidence =0.6)
+    print((findImage))
+    pag.click(findImage)
+except pag.ImageNotFoundException:
+    print('ImageNotFoundException: image not found')
